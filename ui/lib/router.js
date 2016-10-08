68e6196d6bd189c691b9e4ae8a3204e14c4ee708
brennangw <brennangoffwallace@gmail.com>
2016-10-08 13:32:22 -0400

    added routing and application templates

---
+++ ui/lib/router.js
@@ -0,0 +1,26 @@
+Router.configure({
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
+    landingTemplate: 'landing',
+});
+
+var requireLogin = function() {
+    if (Meteor.user()) {
+        this.next();
+    } else {
+        if (Meteor.loggingIn()) {
+            this.render('loading');
+        }
+        this.render('notLoggedIn');
+    }
+};
+
+
+
+//hooks
+Router.onBeforeAction(requireLogin, {only: 'landing'});
+
+Router.route('/', {
+    name: 'landing',
+});
