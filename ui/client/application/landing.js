a8988592fe326f4e640ca3c22bea181d9384e44c
nyletara <iguru95@gmail.com>
2016-10-18 17:20:52 -0400

    Modified form capabilities to Modal

--- ui/client/application/landing.js
+++ ui/client/application/landing.js
@@ -11,3 +11,24 @@ Template.landing.helpers({
         return Replies.find({});
     }
 });
+
+Template.landing.events({
+    'submit .submitOrderForm'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const symbol = target.etf_symbol.value;
+        const orderQty = target.orderQty.value;
+        const strategy = target.strategy.value;
+        const account = target.account.value;
+
+        console.log(event);
+
+        // TODO Here we would make the REST call that starts up the Node-exchange server
+
+        // Clear form
+        target.text.value = '';
+    }
+});
