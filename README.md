bfbb90488e133fe991dc5a878f8aeb338c66975e
nyletara <iguru95@gmail.com>
2016-10-30 15:01:22 -0400

    Successfully parsing data from modal + making HTTP call to Node server with requirements

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 To connect to cloud database add following environment variable during meteor app startup:
 
-  MONGO_URL=mongodb://<dbuser>:<dbpassword>@ds057476.mlab.com:57476/dev
+  MONGO_URL=mongodb://dbuser:dbpassword@ds057476.mlab.com:57476/dev
   
   Contact bgw2119 at columbia dot edu or ig2333 at columbia dot edu for dbusername and dbpassword
 
--- ui/client/application/landing.js
+++ ui/client/application/landing.js
@@ -1,28 +1,51 @@
 
 //Landing page helper functions
-Replies = Meteor.Collection("replies");
+// Replies = Meteor.Collection("replies");
 
 Template.landing.helpers({
-    replies : function () {
-        return Replies.find({});
-    }
+    // replies : function () {
+    //     return Replies.find({});
+    // }
+});
+
+Template.landing.events({
+    // 'submit form': function(event) {
+    //     console.log("TEST")
+    // }
 });
 
 Template.createOrderForm.events({
     'submit .submitOrderForm': function(event) {
-        // Prevent default browser form submit
         event.preventDefault();
 
-        // Get value from form element
-         var symbol = event.target.etf_symbol.value;
-         var orderQty = event.target.orderQty.value;
-         var strategy = event.target.strategy.value;
-         var account = event.target.account.value;
+        // console.log(event.type);
+        // console.log("Form submitted");
 
-        // Clear form
-        symbol = '';
-        orderQty = '';
-        strategy = '';
-        account = '';
+        $('#createOrderModal').modal('hide');
+
+        var target = event.target;
+        var symbol = target.etf_symbol.value;
+        var orderQty = target.orderQty.value;
+        var strategy = target.strategy.value;
+        var account = target.account.value;
+        var side;
+
+        if(target.sell_button.value) {
+            side = "sell";
+        }
+
+        console.log(symbol);
+        console.log(orderQty);
+        console.log(strategy);
+        console.log(account);
+        console.log(side);
+
+        //Call the request to the endpoint here
+
+        Meteor.call("sendTradeRequest", symbol, orderQty, side, strategy, account, function(error, results) {
+            // console.log(results.content); //results.data should be a JSON object
+            console.log("Sent a trade request");
+        });
+
     }
 });
