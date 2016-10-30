c9079d597d19328867aee0a6b8585432fdc8b037
nyletara <iguru95@gmail.com>
2016-10-30 15:17:52 -0400

    Debugging statements for sendTradeRequest server function

--- ui/server/main.js
+++ ui/server/main.js
@@ -4,15 +4,25 @@ Meteor.startup(function() {
     Meteor.methods({
     sendTradeRequest: function(etfSymbol, orderQty, side, tradeStrategy, accountNumber) {
         this.unblock();
-        console.log("Sending request to node server");
+        console.log("Sending request to node server. Parameters:");
+        console.log(etfSymbol);
+        console.log(orderQty);
+        console.log(side);
+        console.log(tradeStrategy);
+        console.log(accountNumber);
 
-        var request = Meteor.http.call("GET", "http://localhost:8081/?id=3&" +
-            "qty=" + orderQty +
-            "&side=" + side +
-            "&price=100" +
-            "&interval=*/1%20*%20*%20*%20*%20*" +
-            "&iters=4");
+        try {
+            var request = Meteor.http.call("GET", "http://localhost:8081/?id=3&" +
+                "qty=" + orderQty +
+                "&side=" + side +
+                "&price=100" +
+                "&interval=*/1%20*%20*%20*%20*%20*" +
+                "&iters=4");
 
+        }
+        catch(err){
+            console.log(err.message);
+        }
         // return request;
         console.log("Made it to the end of the call");
     }
