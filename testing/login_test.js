81ed564a74a457844bc06ca86c9385918d859375
brennangw <brennangoffwallace@gmail.com>
2016-11-05 16:17:30 -0400

    added two tests for logging in

--- testing/login_test.js
+++ testing/login_test.js
@@ -6,3 +6,15 @@ Scenario('arrive at homepage and get instructions', (I) => {
     I.see('You must login to use the system');
     I.see('Sign in');
 });
+
+Scenario('can login from the homepage', (I) => {
+    I.amOnPage('http://localhost:3000/');
+    I.click('Sign in');
+    I.see('Email');
+    I.see('Password');
+    I.fillField('Email', 'user@test.com');
+    I.fillField('Password', '123456');
+    I.click('#login-buttons-password');
+    I.seeElement({id: 'placeOrder'});
+
+});
