4aee176f976736d4229128f63b649dfdff5a1b90
guptakanksha1992 <guptakanksha1992@gmail.com>
2016-11-09 22:33:18 -0500

    Updated test cases

--- testing/login_test.js
+++ testing/login_test.js
@@ -1,7 +1,7 @@
 
 Feature('Unit Test Cases');
 
-Scenario('arrive at homepage and get instructions', (I) => {
+Scenario('Arrive at homepage and get instructions', (I) => {
     I.amOnPage('http://localhost:3000/');
     I.see('You must login to use the system');
 });
@@ -10,7 +10,6 @@ Scenario('arrive at homepage and get instructions', (I) => {
 Scenario('Forgot password feature', (I) => {
     I.amOnPage('http://localhost:3000/');
 I.see('You must login to use the system');
-I.see('Sign in');
 I.click('Sign in');
 I.click('Forgot password');
 I.see('Reset password');
@@ -28,6 +27,43 @@ I.click('Create account');
 I.see('You must login to use the system');
 });
 
+Scenario('Cannot login from the homepage if entered credentials are incorrect', (I) => {
+    I.amOnPage('http://localhost:3000/');
+I.click('Sign in');
+I.see('Email');
+I.see('Password');
+I.fillField('Email', 'akacheck11@test.com');
+I.fillField('Password', '1234561');
+I.click('#login-buttons-password');
+I.see('You must login to use the system');
+I.see('Sign in');
+});
+
+
+
+Scenario('Can login from the homepage- when a valid user exists with given credentials', (I) => {
+    I.amOnPage('http://localhost:3000/');
+    I.click('Sign in');
+    I.see('Email');
+    I.see('Password');
+    I.fillField('Email', 'aka@test.com');
+    I.fillField('Password', '123456');
+    I.click('#login-buttons-password');
+    I.seeElement('#placeOrder');
+
+});
+
+Scenario('Place trade', (I) => {
+    I.click('Sign in');
+ I.see('Email');
+ I.see('Password');
+ I.fillField('Email', 'aka@test.com'); 
+I.fillField('Password', '123456');
+ I.click('#login-buttons-password');
+I.seeElement('#placeOrder');
+I.click('Place Order');
+I.see('Parent Trade ID Quantity Side Average Price Execution Status Time');
+});
 
 
 Scenario('Create new account', (I) => {
@@ -38,67 +74,16 @@ I.click('Sign in');
 I.see('Email');
 I.see('Password');
 I.click('Create account');
-I.fillField('Email', 'aka11511@test.com');
-I.fillField('Password', '123456');
-I.click('Create account');
-I.seeElement({id: 'placeOrder'});
-});
-
-
-
-Scenario('cannot login from the homepage if entered credentials are incorrect', (I) => {
-    I.amOnPage('http://localhost:3000/');
-I.click('Sign in');
-I.see('Email');
-I.see('Password');
-I.fillField('Email', 'aka11@test.com');
-I.fillField('Password', '1234561');
+I.fillField('Email', 'rdvvpem1@test.com');
+I.fillField('Password', '1234567');
 I.click('#login-buttons-password');
-I.see('You must login to use the system');
-I.see('Sign in');
-
+I.seeElement('#placeOrder');
 });
-
-Scenario('can login from the homepage- when a valid user exists with given credentials', (I) => {
-    I.amOnPage('http://localhost:3000/');
-    I.click('Sign in');
-    I.see('Email');
-    I.see('Password');
-    I.fillField('Email', 'aka@test.com');
-    I.fillField('Password', '123456');
-    I.click('#login-buttons-password');
-    I.seeElement({id: 'placeOrder'});
-
-});
-
-Scenario('Place trade', (I) => {
-    I.amOnPage('http://localhost:3000/');
-I.click('Sign in');
-I.see('Email');
-I.see('Password');
-I.fillField('Email', 'aka@test.com');
-I.fillField('Password', '123456');
-I.click('#login-buttons-password');
-I.seeElement({id: 'placeOrder'});
-I.click({id: 'placeOrder'});
-I.click({id: 'sell_button'});
-I.see('Parent Trade ID Quantity Side Average Price Execution Status Time');
-});
-
-
-
-
 
 
 Scenario('Logout from the system', (I) => {
     I.amOnPage('http://localhost:3000/');
-I.amOnPage('http://localhost:3000/');
-I.click('Sign in');
-I.see('Email');
-I.see('Password');
-I.fillField('Email', 'aka@test.com');
-I.fillField('Password', '123456');
-I.click('#login-buttons-password');
-I.click('#login-dropdown-list');
-
+I.click('#login-buttons');
+I.click('#login-buttons-logout');
+I.see('You must login to use the system');
 });
