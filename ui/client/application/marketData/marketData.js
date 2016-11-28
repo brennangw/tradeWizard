629f6ebd7ab8ad5500abfe53a2c47b2fba2a06a7
brennangw <brennangoffwallace@gmail.com>
2016-11-28 01:51:30 -0600

    commented out market data info, table works again

--- ui/client/application/marketData/marketData.js
+++ ui/client/application/marketData/marketData.js
@@ -1,11 +1,11 @@
-import { MarketData } from '../../../lib/collections/marketData.js';
-
-Template.marketData.onCreated(function bodyOnCreated() {
-    // this.subscribe("marketData");
-});
-
-Template.marketData.helpers({
-    data : function() {
-        return MarketData.findOne({});
-    }
-});
+// import { MarketData } from '../../../lib/collections/marketData.js';
+//
+// Template.marketData.onCreated(function bodyOnCreated() {
+//     // this.subscribe("marketData");
+// });
+//
+// Template.marketData.helpers({
+//     data : function() {
+//         return MarketData.findOne({});
+//     }
+// });
--- ui/lib/collections/marketData.js
+++ ui/lib/collections/marketData.js
@@ -1,12 +1,12 @@
-import { Mongo } from 'meteor/mongo';
-export const MarketData = new Mongo.Collection("ping_storage");
-
-if (Meteor.isServer) {
-    // This code only runs on the server
-    // Only publish tasks that are public or belong to the current user
-    Meteor.publish('marketData', function marketDataPublication() {
-        return MarketData.find({});
-    });
-} else if (Meteor.isClient) {
-    Meteor.subscribe('marketData');
-}
+// import { Mongo } from 'meteor/mongo';
+// export const MarketData = new Mongo.Collection("ping_storage");
+//
+// if (Meteor.isServer) {
+//     // This code only runs on the server
+//     // Only publish tasks that are public or belong to the current user
+//     Meteor.publish('marketData', function marketDataPublication() {
+//         return MarketData.find({});
+//     });
+// } else if (Meteor.isClient) {
+//     Meteor.subscribe('marketData');
+// }
