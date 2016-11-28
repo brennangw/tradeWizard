32cc3f408d4d756715c345060d7d9fa3358e3525
brennangw <brennangoffwallace@gmail.com>
2016-11-27 22:06:29 -0600

    pinger can now send mutiple requests for different ids

--- pinger/main.js
+++ pinger/main.js
@@ -1,3 +1,4 @@
+
 const mongojs = require('mongojs');
 const cron = require('node-cron');
 var request = require("request");
@@ -6,7 +7,7 @@ const exchangeLocation = process.argv[2];
 const mongoLocation = process.argv[3];
 var db = mongojs(mongoLocation);
 const pingStorage = db.collection('ping_storage');
-const ids = [1];
+const ids = [1,2,3];
 
 const saveResponse = function(error, response, body) {
     pingStorage.save(JSON.parse(body), function(err,doc) {
@@ -15,13 +16,15 @@ const saveResponse = function(error, response, body) {
             console.log(err);
         }
     });
-}
+};
 
-var cronPing = cron.schedule('*/1 * * * * *', function () {
+var cronPing = cron.schedule('*/15 * * * * *', function () {
+    var sleep = require('sleep');
     var index;
     for (index = 0; index < ids.length; index++) {
         console.log("sent request for id" + ids[index]);
         request(exchangeLocation + '/query?id=' + ids[index], saveResponse);
+        sleep.sleep(2);
     }
 }, false);
 
