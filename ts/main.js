13b2730551f602f1ef557a5442df4b34abe84a73
Stan Peceny <skp2140@columbia.edu>
2016-12-01 04:05:26 +0100

    main.js edits

--- ts/main.js
+++ ts/main.js
@@ -4,9 +4,9 @@ const exchangeLocation = process.argv[3];
 const mongoLocation = process.argv[4];
 
 //get classes
-const Exchange = require('./classes/exchange.js');
-const ReceiverServer = require('./classes/server.js');
-const Database = require('./classes/database.js');
+const Exchange = require("./classes/exchange.js");
+const ReceiverServer = require("./classes/server.js");
+const Database = require("./classes/database.js");
 
 //set up class instances
 var db = new Database(mongoLocation);
