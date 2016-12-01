def23b3e59ab095c60eb310b3da6bf0cfdcc65a4
Stan Peceny <skp2140@columbia.edu>
2016-12-01 04:12:11 +0100

    database.js edits

--- ts/classes/database.js
+++ ts/classes/database.js
@@ -1,21 +1,20 @@
 //connect to db
 
-const Router = require('router');
-const mongojs = require('mongojs');
+const Router = require("router");
+const mongojs = require("mongojs");
 
 var Database = function (db_url) {
     this.mongoConnection = mongojs(db_url);
-    this.replies = this.mongoConnection.collection('replies_from_the_exchange');
-    this.sent = this.mongoConnection.collection('trades_sent_to_exchange');
-    this.parents = this.mongoConnection.collection('parents');
-    this.currentPid = this.mongoConnection.collection('pid');
+    this.replies = this.mongoConnection.collection("replies_from_the_exchange");
+    this.sent = this.mongoConnection.collection("trades_sent_to_exchange");
+    this.parents = this.mongoConnection.collection("parents");
+    this.currentPid = this.mongoConnection.collection("pid");
     var that = this;
-    this.currentPid.findOne({}, function(err, doc) {
+    this.currentPid.findOne({}, function (err, doc) {
         if (!doc) {
             that.currentPid.save({pid: 1});
         }
     });
 };
-
 
 module.exports = Database;
