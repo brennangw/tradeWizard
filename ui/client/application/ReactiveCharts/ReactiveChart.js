6f660330a817760317ef38fe43102aeea2bdd3f7
guptakanksha1992 <guptakanksha1992@gmail.com>
2016-12-11 00:38:56 -0500

    Implemented Order distribution, child and parents orders- pie chart

--- ui/client/application/ReactiveCharts/ReactiveChart.js
+++ ui/client/application/ReactiveCharts/ReactiveChart.js
@@ -3,7 +3,7 @@
  */
 
 import { Replies } from '../../../lib/collections/repliesCollection.js';
-
+import { RepliesAggregate } from '../../../lib/collections/repliesCollection.js';
 
     // Meteor.subscribe("Tasks");
     var Highcharts = require('highcharts');
@@ -14,24 +14,35 @@ Template.ReactiveChart.helpers({
 
         createChart: function () {
 
+            // Getting data for charts
+
+            var time = Replies.find({},{timestamp: 1});
+            var quan = Replies.find({},{qty: 1});
+            console.log("my time"+time);
+            console.log("my quan"+quan);
             Meteor.defer(function() {
             Highcharts.chart('chart', {
                 chart: {
                     renderTo: 'chart',
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: true,
                     type: 'line'
                 },
                 title: {
-                    text: 'Monthly Average Temperature'
+                    text: 'Quantity vs Time'
                 },
                 subtitle: {
-                    text: 'Source: WorldClimate.com'
+                    text: 'Source: TradeWizard'
                 },
                 xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                    title: {
+                        text: 'TimeStamp'
+                    }
                 },
                 yAxis: {
                     title: {
-                        text: 'Temperature (°C)'
+                        text: 'Quantity Traded'
                     }
                 },
                 plotOptions: {
@@ -43,8 +54,8 @@ Template.ReactiveChart.helpers({
                     }
                 },
                 series: [{
-                    name: 'Tokyo',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                    name: 'Time',
+                    data: [quan]
                 }, {
                     name: 'London',
                     data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
@@ -63,13 +74,16 @@ Template.ReactiveChart.helpers({
             Highcharts.chart('chart2', {
                 chart: {
                     renderTo: 'chart',
+                    plotBackgroundColor: true,
+                    plotBorderWidth: null,
+                    plotShadow: true,
                     type: 'column'
                 },
                 title: {
                     text: 'Side(Buy/Sell) Count'
                 },
                 subtitle: {
-                    text: 'Source: TradeWizards'
+                    text: 'Source: TradeWizard'
                 },
                 xAxis: {
                     categories: ['Trades'],
@@ -101,7 +115,107 @@ Template.ReactiveChart.helpers({
                     name: 'Buy',
                     data: [buyquan]
                 }]
+            })});
+        },
+
+    createChart3: function () {
+
+        var childs = Replies.find({}).count();
+        var parents = RepliesAggregate.find({}).count();
+
+        var sum=childs+parents;
+
+        if(sum!=0) {
+            var childsfinal = (childs / sum) * 100;
+            var parentsfinal = (parents / sum) * 100;
+        }
+        else {
+            var childsfinal = 0;
+            var parentsfinal = 0;
+        }
+
+        Meteor.defer(function() {
+            Highcharts.chart('chart3', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Orders Distribution'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    name: 'Parent Orders',
+                    colorByPoint: true,
+                    data: [{
+                        name: 'Child Orders',
+                        y: childsfinal
+                    }, {
+                        name: 'Parent Orders',
+                        y: parentsfinal,
+                        sliced: true,
+                        selected: true
+                    }] }] })});
+
+},
+
+    createChart4: function () {
+
+
+        Meteor.defer(function() {
+            Highcharts.chart('chart4', {
+                chart: {
+                    renderTo: 'chart',
+                    plotBackgroundColor: true,
+                    plotBorderWidth: null,
+                    plotShadow: true,
+                    type: 'line'
+                },
+                title: {
+                    text: 'Monthly Average Temperature'
+                },
+                subtitle: {
+                    text: 'Source: WorldClimate.com'
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Temperature (°C)'
+                    }
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                series: [{
+                    name: 'Tokyo',
+                    data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                }, {
+                    name: 'London',
+                    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                }]
             })  }); }
 
 
-      });
+
+});
