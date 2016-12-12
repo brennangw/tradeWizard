e148a64af09e9c0d8e50c42089cab9cdbe495422
guptakanksha1992 <guptakanksha1992@gmail.com>
2016-12-12 02:14:54 -0500

    Added spread for market data

--- ui/client/application/marketData/marketData.js
+++ ui/client/application/marketData/marketData.js
@@ -6,8 +6,16 @@ Template.marketData.onCreated(function bodyOnCreated() {
 
 Template.marketData.helpers({
     data : function() {
-       // console.log("I am being called");
+        // console.log("I am being called");
         return MarketData.findOne({});
 
+    },
+    get_Spread : function(top_bid, top_ask) {
+        // console.log("I am being called");
+        var spreadval= Number (Math.abs(top_bid-top_ask)).toFixed(2);
+        return spreadval;
+
     }
+
+
 });
